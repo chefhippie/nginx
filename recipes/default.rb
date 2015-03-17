efb0c826d2c01722ab143db85bf36a795fbb079a
Thomas Boerger <tboerger@suse.de>
2015-03-17 11:45:34 +0100

    Refresh the repos after added

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ when "suse"
     key node["nginx"]["zypper"]["key"]
     title node["nginx"]["zypper"]["title"]
 
-    action :add
+    action [:add, :refresh]
 
     only_if do
       node["nginx"]["zypper"]["enabled"]
